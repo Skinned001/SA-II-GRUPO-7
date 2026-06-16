a6ebed477db1892aeccc2a2645edf3bd2c92e874
¨lautaro <shortlauty35@gmail.com>
2026-06-16 14:56:26 -0300

    proba ahora fern por 2

--- movenet/app.js
+++ movenet/app.js
@@ -1,13 +1,44 @@
-const video = document.getElementById("video");
+import * as tf from "https://esm.sh/@tensorflow/tfjs@4.22.0";
 
-async function iniciarCamara() {
+async function iniciar() {
+  await tf.setBackend("webgl");
+  await tf.ready();
+
+  console.log("TensorFlow OK");
+
+  const model = await tf.loadGraphModel(
+    "https://tfhub.dev/google/tfjs-model/movenet/singlepose/lightning/4",
+    { fromTFHub: true },
+  );
+
+  console.log("MoveNet cargado");
+
+  const video = document.getElementById("video");
+
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
   });
 
   video.srcObject = stream;
 
-  console.log("Camara iniciada");
-}
+  await video.play();
 
-iniciarCamara();
+  console.log("Cámara iniciada");
+}
+setInterval(async () => {
+  const tensor = tf.browser.fromPixels(video);
+
+  const resized = tf.image.resizeBilinear(tensor, [192, 192]);
+
+  const input = resized.expandDims(0);
+
+  const prediction = await model.executeAsync(input);
+
+  console.log(prediction);
+
+  tensor.dispose();
+  resized.dispose();
+  input.dispose();
+}, 1000);
+
+iniciar();
