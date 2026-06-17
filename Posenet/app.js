cc016ca7a064c811ccadd96e3b461c2cf5bd7fd5
¨lautaro <shortlauty35@gmail.com>
2026-06-17 09:59:28 -0300

    se añadio la carpeta de posenet con sus archivos necesario para que funcione

---
+++ Posenet/app.js
@@ -0,0 +1,49 @@
+import * as tf from "https://esm.sh/@tensorflow/tfjs@4.22.0";
+import * as posenet from "https://esm.sh/@tensorflow-models/posenet";
+
+async function iniciar() {
+  await tf.setBackend("webgl");
+  await tf.ready();
+
+  console.log("TensorFlow OK");
+
+  const net = await posenet.load();
+
+  console.log("PoseNet cargado");
+
+  const video = document.getElementById("video");
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+  });
+
+  video.srcObject = stream;
+
+  await video.play();
+
+  const canvas = document.getElementById("canvas");
+
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  const ctx = canvas.getContext("2d");
+  async function detectar() {
+    const pose = await net.estimateSinglePose(video);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const punto of pose.keypoints) {
+      if (punto.score > 0.3) {
+        ctx.beginPath();
+
+        ctx.arc(punto.position.x, punto.position.y, 8, 0, Math.PI * 2);
+
+        ctx.fillStyle = "blue";
+        ctx.fill();
+      }
+    }
+    requestAnimationFrame(detectar);
+  }
+
+  detectar();
+}
+
+iniciar();
